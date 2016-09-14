8d22e110422a0b1045a2f33378fdf37ade7cc4b6
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-09-14 16:35:31 -0400

    Added a info Window to the maps in order to provide more information about cafe-roy

--- contact.js
+++ contact.js
@@ -1,13 +1,29 @@
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 39.6400, lng: -86.8630},
+    zoom: 16,
+  });
+  var marker = new google.maps.Marker({
+   position: {lat: 39.6409, lng: -86.86357},
+   map: map,
+   title: 'Cafe Roy',
+ });
+ var contentString = '<div id="content">'+
+     '<div id="siteNotice">'+
+     '</div>'+
+     '<h1 id="firstHeading" class="firstHeading">Cafe Roy</h1>'+
+     '<div id="bodyContent">'+
+     '<p><b>Cafe Roy</b>, is located on the northeastern part of Roy\'O West Library.  </p>'+
+     '<a href="index.html">Home</a> '+
 
-var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 39.6400, lng: -86.8630},
-          zoom: 16,
-        });
-        var marker = new google.maps.Marker({
-         position: {lat: 39.6408553, lng: -86.86370},
-         map: map,
-         title: 'Cafe Roy'
-       });
-      }
+     '</div>'+
+     '</div>';
+ var infowindow = new google.maps.InfoWindow({
+   content: contentString
+ });
+
+ marker.addListener('click', function() {
+   infowindow.open(map, marker);
+ });
+
+}
